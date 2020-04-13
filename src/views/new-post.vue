29617b26bc0849bc835e82ab367d2c9f17c4b6ef
Lyle <494205736@qq.com>
2020-04-13 22:29:23 +0800

    '文章发布页面组件'

--- src/views/new-post.vue
+++ src/views/new-post.vue
@@ -1,10 +1,114 @@
 <template>
-  <div>文章发布</div>
+  <div>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="标题">
+        <el-input v-model="form.title"></el-input>
+      </el-form-item>
+
+      <!-- 单选框 -->
+      <el-form-item label="类型">
+        <el-radio-group v-model="form.type">
+          <el-radio :label="1">文章</el-radio>
+          <el-radio :label="2">视频</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 视频上传 -->
+      <el-form-item label="视频文件" v-if="form.type === 2">
+        <el-upload
+          class="upload-demo"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-remove="handleRemove"
+          :limit="1"
+        >
+          <el-button size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">
+            只能上传mp4,avi文件，且不超过2M
+          </div>
+        </el-upload>
+      </el-form-item>
+      <!-- 多选框 -->
+      <el-form-item label="栏目">
+        <el-checkbox-group v-model="form.category">
+          <el-checkbox label="A" name="type"></el-checkbox>
+          <el-checkbox label="B" name="type"></el-checkbox>
+          <el-checkbox label="C" name="type"></el-checkbox>
+          <el-checkbox label="D" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <!-- 单选，单选使用el-radio-group，文档地址：https://element.eleme.cn/#/zh-CN/component/form#biao-dan-yan-zheng
+			里面的特殊资源 -->
+      <el-form-item label="内容" v-if="form.type === 1" class="editor">
+        <vue-editor v-model="form.content"></vue-editor>
+      </el-form-item>
+
+      <!-- 图片上传 -->
+
+      <el-form-item label="封面上传" v-if="form.type === 1">
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="photoRemove"
+        >
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="form.dialogVisible">
+          <img width="100%" :src="form.dialogImageUrl" alt="" />
+        </el-dialog>
+      </el-form-item>
+      <!-- 提交按钮 -->
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {};
+import { VueEditor } from "vue2-editor";
+export default {
+  components: {
+    VueEditor
+  },
+  data() {
+    return {
+      // 表单的数据对象
+      form: {
+        title: "",
+        // type: 1,
+        content: "<h1>Some initial content</h1>",
+        // 控制默认按钮选择,点击按钮type变化为按钮label值
+        type: 1,
+        category: [],
+        dialogImageUrl: "",
+        dialogVisible: false
+      }
+    };
+  },
+  methods: {
+    //  预览图片的事件
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    // 移除已经上传的图片
+    photoRemove() {},
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    },
+    onSubmit() {}
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.editor /deep/ .el-form-item__content {
+  line-height: 0;
+}
 </style>
